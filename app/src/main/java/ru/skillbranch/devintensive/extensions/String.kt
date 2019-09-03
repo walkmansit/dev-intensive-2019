bd679981ef8f6a70855496335e616e526172c041
AndreyNagaev <walkmansit@gmail.com>
2019-09-03 18:24:40 +0400

    strip html fix

--- app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -5,7 +5,6 @@ fun String.truncate(trunLength:Int = 16) = if (this.trimEnd().length > trunLengt
 
 const val OPEN_TAG = '<'
 const val CLOSE_TAG = '>'
-//const val ESCAPE_SYMBOL = '&'
 const val EMPTY_SYMBOL = ' '
 
 const val amp = "&amp;"
@@ -13,12 +12,6 @@ const val lt = "&lt;"
 const val qt = "&gt;"
 const val quot = "&quot;"
 const val apos = "&apos;"
-
-//&amp; → & (ampersand, U+0026)
-//&lt; → < (less-than sign, U+003C)
-//&gt; → > (greater-than sign, U+003E)
-//&quot; → " (quotation mark, U+0022)
-//&apos; → ' (apostrophe, U+0027)
 
 fun String.stripHtml():String{
     val sb = StringBuilder()
@@ -41,6 +34,24 @@ fun String.stripHtml():String{
 
         prevChar = c
     }
-    return sb.toString().replace(amp,"").replace(lt,"").replace(qt,"")
+    return sb.toString().removeExtraWhiteSpaces().replace(amp,"").replace(lt,"").replace(qt,"")
         .replace(quot,"").replace(apos,"")
+}
+
+fun String.removeExtraWhiteSpaces(): String {
+
+    var result = ""
+
+    var prevChar = ""
+
+    for ( char in this ) {
+        if ( (prevChar == " " && char == ' ').not() ) {
+
+            result += char
+        }
+
+        prevChar = char.toString()
+    }
+
+    return result
 }
